bf56cf8414fabd24feff1ddff6e324157ab16f89
Тимофей Савицкий <tamasav@yandex.ru>
2024-12-04 18:35:14 +0000

    for max

--- lambda_search/homepage/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-__all__ = ()
-
-
-class HomepageConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "homepage"
-    verbose_name = "Домашняя страница"
--- lambda_search/homepage/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic import TemplateView
-
-__all__ = ()
-
-
-class HomeView(TemplateView):
-    template_name = "homepage/main.html"
